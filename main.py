5447ff7518b021f0d22acf57c767f398491dbf16
s-mbk <somtochukwumbuko@gmail.com>
2023-05-11 12:51:44 +0200

    updates

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 
 app = FastAPI()
 
+# view patient data
 
 @app.get("/")
 async def root():
@@ -16,3 +17,28 @@ async def root():
             patients.append(json.load(file))
 
     return patients
+
+
+# add new patient data
+
+@app.post("/patients/create")
+async def patient_create(jsonData):
+    # How to secure our "post-box"?
+    # Check for valid JSON
+    try:
+        patient = json.loads(jsonData)
+    except ValueError as err:
+        return False
+
+    # TODO: Check header for the right Content-Type: application/json+fhir
+    
+    # Giving the newly created file (patient) a file name (patient id) in continuation of already existing files (patient ids)
+    file_id = len(os.listdir('fhir_resources')) + 1
+    filename = f"patient_{file_id}.json"
+
+    with open(filename, 'w') as file:
+        json.dump(patient, file)
+        # json.dump() converts the Python objects into appropriate json objects
+
+    return 'OK'
+    
